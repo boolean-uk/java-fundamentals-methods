7783b555bc0a07e5b3528cda9241c9c2965d25a2
Aidan van Geest <aidanvangeest@gmail.com>
2023-04-26 19:47:56 +0200

    Standard criteria completed

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -56,7 +56,8 @@ public class Exercise extends ExerciseBase {
 
     /*
         3. Construct an array of numbers
-        Create a method named constructNumberArray that accepts two whole numbers named lower and upper.
+        Create a method named constructNumberArray
+        that accepts two whole numbers named lower and upper.
         The method must return an array containing all the whole numbers between lower and upper,
         including lower and upper. Example input and output:
 
@@ -66,14 +67,29 @@ public class Exercise extends ExerciseBase {
         10, 13 | [10,11,12,13]
         -1, 1  | [-1,0,1]
      */
-
+        public int[] constructNumberArray (int lower, int upper) { //Create a method that accepts two integers and will return an array
+            //lower = 10, upper = 13
+            int length = upper - lower; //determine length to know how many times to loop through the array
+            int count = lower; //create counter variable to keep track of our position in the array (starting from lower)
+            int[] array = new int[length+1]; //create an empty array, specify how many numbers will be in that array (length+1)
+            for (int i = 0; i <= length; i++) { //create for loop that runs through array. i starts at index 0 and ends at length
+                array[i] = count; //At index of i, insert count value into array
+                count++; //increases count by one
+            }
+            // array[0] = 10;
+            // array[1] = 11;
+            // array[2] = 12;
+            // array[3] = 13;
+            return array;
+        }
 
 
 
     /*
         4. Shout at a dev
         Create a method named shout that accepts a string and a whole number.
-        The method must return the same string in upper case with exclamation marks (!) appended to the end.
+        The method must return the same string in upper case
+        with exclamation marks (!) appended to the end.
         The number of exclamation marks appended must be determined by the number provided.
         Example input and output:
 
@@ -83,7 +99,14 @@ public class Exercise extends ExerciseBase {
         error, 10   | ERROR!!!!!!!!!!
      */
 
+        public String shout (String word, int number) { //create a method named shout that will return a String and accepts a String and an integer
+            String exclamations = ""; //Create an empty string to hold the exclamations (!)
 
+            for (int i = 1; i <= number; i++) { //create a loop starting at 1 and ending at number to add the exclamations
+                exclamations = exclamations + "!"; //In each loop, add an extra exclamation, until we have reached the number
+            }
+            return word.toUpperCase() + exclamations; //returns the string in uppercase followed by the exclamations
+        }
 
 
 }
