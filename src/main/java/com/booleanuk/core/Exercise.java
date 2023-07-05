6cd0728d5e831ef49693316ee50c364793b68ac1
Damian Gurdak <damian.gurdak@talan.com>
2023-07-05 15:16:12 +0200

    3,4

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -2,6 +2,8 @@ package com.booleanuk.core;
 
 import com.booleanuk.helpers.ExerciseBase;
 
+import java.util.stream.IntStream;
+
 public class Exercise extends ExerciseBase {
     /*
         A method is a function, a single piece of logic that can run. In Java, a class is a convenient
@@ -65,6 +67,11 @@ public class Exercise extends ExerciseBase {
         -1, 1  | [-1,0,1]
      */
 
+    public int[] constructNumberArray(int lower, int upper) {
+        return IntStream.rangeClosed(lower, upper).toArray();
+    }
+
+
 
 
 
@@ -80,6 +87,10 @@ public class Exercise extends ExerciseBase {
         disaster, 5 | DISASTER!!!!!
         error, 10   | ERROR!!!!!!!!!!
      */
+
+    public String shout(String word, int exclamations) {
+        return word.toUpperCase() + ("!").repeat(exclamations);
+    }
 
 
 
