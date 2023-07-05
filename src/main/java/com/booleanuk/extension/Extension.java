8d3346a47a0efa35bc703f7be3098c87a11e0dc2
Lev Moskaliuk <moskaluklev@gmail.com>
2023-07-05 13:45:01 +0200

    All extensions are completed

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -49,7 +49,9 @@ public class Extension extends ExtensionBase {
         which is the sum of the preparation time and the number of minutes it's been
         in the oven. Use your calculatePrepTime method in the calculation
      */
-
+    public int totalTimeSpent (int layers, int mins) {
+        return calculatePrepTime(layers) + mins;
+    }
 
 
 
