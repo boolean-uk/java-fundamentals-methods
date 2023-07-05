4b2028f4430645ece6989e8b10112edc3cb5a4ad
Jan Rusak <jrusak001@gmail.com>
2023-07-05 15:16:09 +0200

    Added implementation to 6 and 8 in Extension

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -20,6 +20,9 @@ public class Extension extends ExtensionBase {
      * It must return how many minutes are left to bake based on the input
      * and the result of calling the bakingTime method
      */
+    public int remainingBakeTime(int done) {
+        return bakingTime() - done;
+    }
 
     /*
      * 7. Create a method named calculatePrepTime that accepts one input:
@@ -43,5 +46,7 @@ public class Extension extends ExtensionBase {
      * which is the sum of the preparation time and the number of minutes it's been
      * in the oven. Use your calculatePrepTime method in the calculation
      */
-
+    public int totalTimeSpent(int layers, int done) {
+        return calculatePrepTime(layers) + done;
+    }
 }
