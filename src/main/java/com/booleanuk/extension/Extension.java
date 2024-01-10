7346342a42faaea2896547b70ca64985bfa4157e
Alexander Niklasson <alexander.niklasson@live.com>
2024-01-10 12:46:08 +0100

    Finished Exercise!

--- src/main/java/com/booleanuk/extension/Extension.java
+++ src/main/java/com/booleanuk/extension/Extension.java
@@ -9,7 +9,9 @@ public class Extension extends ExtensionBase {
         5. Create a method named bakingTime that returns the number 50
      */
 
-
+    public int bakingTime(){
+        return 50;
+    }
 
 
     /*
@@ -20,7 +22,10 @@ public class Extension extends ExtensionBase {
         and the result of calling the bakingTime method
      */
 
+    public int remainingBakeTime(int minutes){
 
+        return bakingTime() - minutes;
+    }
 
 
     /*
@@ -31,7 +36,9 @@ public class Extension extends ExtensionBase {
         each layer taking 3 minutes to prepare
      */
 
-
+    public int calculatePrepTime(int layers){
+        return 3*layers;
+    }
 
 
     /*
@@ -45,6 +52,8 @@ public class Extension extends ExtensionBase {
      */
 
 
-
+    public int totalTimeSpent(int layers, int minutes){
+        return calculatePrepTime(layers) + minutes;
+    }
 
 }
